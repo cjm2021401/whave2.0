c48e81fe0df6c2cf1a075468e5181c187b6c20fb
cjm2021401 <cjm2021401@khu.ac.kr>
2020-04-19 16:18:23 +0900

    Updated design and performance

--- js/script.js
+++ js/script.js
@@ -1,65 +1,98 @@
-/*/ Firebase Settings
-var firebaseConfig = {
-    apiKey: 'AIzaSyC4Xgvmc_LU-sCGJmCRLILV3mHjuG5yyhs',
-    authDomain: 'whaledb.firebaseapp.com',
-    databaseURL: 'https://whaledb.firebaseio.com',
-    projectId: 'whaledb',
-    storageBucket: 'whaledb.appspot.com',
-    messagingSenderId: '536005499995',
-    appId: '1:536005499995:web:aa1f0eafc130b2915bf37e',
-};
-var app = firebase.initializeApp(firebaseConfig);
+function createItem() {
+    var nickname = document.forms["myForm"]["fname"].value;
+    if (nickname == "") {
+        alert("카메라 이름을 입력하세요");
+        return false;
+    }
 
-// Google Login
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().signInWithPopup(provider).then(function(result) {
-    // Firestore Access
-    var db = firebase.firestore(app);
-    var docRef = db.collection("users").doc(result.user.uid);
-    docRef.get().then(function(doc) {
-        if (doc.exists) {
-            var cam_no = 0;
-            var cam_active = 'active';
-            for (var cam_name of doc.data().cameras) {
-                $('#v-list').append(`<li data-target="#carousel-example" data-slide-to="${cam_no}" class="${cam_active}"></li>`);
-                $('#v-item').append(`<div class="carousel-item ${cam_active}">
-                    <video-js id="video-${cam_no}" class="embed-responsive embed-responsive-16by9 vjs-default-skin vjs-big-play-centered" data-setup='{"fluid": true}' controls>
-                        <source src="http://wj.khunet.net/live/${result.user.uid}${cam_no}.m3u8" type="application/x-mpegURL">
+    for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        var value = localStorage.getItem(key);
+        if(value==nickname)
+        {
+            alert("중복된 이름의 카메라가 있습니다. \n다른 카메라 이름을 입력하여 주세요.");
+            return false;
+        }
+
+    }
+
+    var password = Date.now();
+    var random = Math.floor(Math.random() * 900) + 100;
+    var timestamp = String(random) + String(password);
+    localStorage.setItem(timestamp, nickname);
+
+    alert("카메라 생성이 완료되었습니다. \n패스워드는 "+timestamp+" 입니다. \n카메라 연결하기를 눌러 설정을 완료해주세요!");
+
+}
+
+
+function clearAll() {
+    var len=localStorage.length;
+    if(len==0)
+    {
+        alert("현재존재 하는 카메라가 없습니다.");
+
+    }
+    localStorage.clear();
+    alert("삭제가 완료되었습니다.");
+    window.location.reload();
+
+
+
+}
+
+function clearOne(idx){
+
+
+    var arr = Array.from(idx.cells);
+    var key = arr[0].innerHTML;
+    localStorage.removeItem(key);
+    alert("삭제가 완료되었습니다.");
+    window.location.reload();
+}
+var hst = document.getElementById("result");
+var len=localStorage.length;
+if(len==0)
+{
+    hst.innerHTML += "<tr><td>" + "no camera" + "</td><td>" + "no camera"+ "</td><td>"+"no camera";
+}
+else{
+    for (var i = 0; i < localStorage.length; i++) {
+        // set iteration key name
+        var key = localStorage.key(i);
+        // use key name to retrieve the corresponding value
+        var value = localStorage.getItem(key);
+
+        hst.innerHTML += "<tr><td>" + key + "</td><td>" + value + "</td><td><button onClick = clearOne(this.parentNode.parentNode)>삭제</button></td></tr>";
+    }}
+
+if(localStorage.length!=0) {
+    var cam_no = 0;
+    var cam_active = 'active';
+
+    for (var j = 0; j < localStorage.length; j++) {
+        var ke=localStorage.key(j);
+        var name=localStorage.getItem(ke);
+        $('#v-list').append(`<li data-target="#carousel-example" data-slide-to="${j}" class="${cam_active}"></li>`);
+        $('#v-item').append(`<div class="carousel-item ${cam_active}">
+                    <video-js id="video-${j}" class="embed-responsive embed-responsive-16by9 vjs-default-skin vjs-big-play-centered" data-setup='{"fluid": true}' controls>
+                        <source src="http://wj.khunet.net/live/${ke}.m3u8" type="application/x-mpegURL">
                     </video-js>
+
                     <div class="carousel-caption">
                         <div class="animated fadeInDown">
-                            <h4 class="h4-responsive">${cam_name}</h4>
+                            <h4 class="h4-responsive">${name}</h4>
                         </div>
                     </div>
                 </div>`);
-                videojs(`video-${cam_no}`, {}, function() { this.reload(); });
-                cam_active = '';
-                cam_no++;
-            }
+        videojs(`video-${cam_no}`, {}, function() { this.reload(); });
+        cam_active = '';
+        cam_no++;
+    }
 
-            if(cam_no==0)
-            {
-                 $("#no-video").prepend('<div class="text-center"><h3 class="display-5"style="color: #ffffff">No Video Available</h3></div>');
-             $("#no-video").prepend('<div class="text-center"><img class="mb-4" src="https://i.ibb.co/pwcXqXT/image.png" alt="" width="250" height="250"></div>');
-            console.log('No Document');
-            }
-            
-        } else {
-            // 여기는 그냥 카메라가 없다.
-            
-              $("#no-video").prepend('<div class="text-center"><h3 class="display-5"style="color: #ffffff">No Video Available</h3></div>');
-             $("#no-video").prepend('<div class="text-center" style="position: relative;"><img class="mb-4" src="images/눈물웨일.png" alt="" width="250" height="250"></div>');
-            console.log('No Document');
-        }
-    }).catch(function(error) {
-        // 
-      
-        console.log('Error!', error);
-    });
-}).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.messeag;
-    var email = error.email;
-    var credential = error.credential;
-});*/
 
+
+
+
+
+}
